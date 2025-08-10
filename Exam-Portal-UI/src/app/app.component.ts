1720b75aa798f5157adf5c30ab4dfe8cf7142dd4
manikanta <>
2025-08-11 01:39:25 +0530

    added navbar and routing for login and signup

--- Exam-Portal-UI/src/app/app.component.ts
+++ Exam-Portal-UI/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Exam-Portal-UI/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButton, MatButtonModule, MatIconButton} from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [MatToolbar,MatIcon,MatIconModule,MatToolbarModule,MatButton,MatIconButton,RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
